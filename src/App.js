dd7ab1cbe46f78da084e0ff11aaade1295c316c8
Ayapi <ayk.24a3y@gmail.com>
2020-10-10 15:43:44 +0900

    routing done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from "react";
-import Top from "./pages/Top"
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Top from "./pages/Top";
+import NavBar from "./components/NavBar";
+import Quiz from "./pages/Quiz";
 
 function App() {
-  return ()
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <div className="container">
+        <Switch>
+          <Route exact path="/" component={Top} />
+          <Route path="/quiz" component={Quiz} />
+          <Route render={() => <h4>not found...</h4>} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/pages/Top.js
+++ src/pages/Top.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Top = () => {
   return (
-    <div>
-      <h2>topだよ！</h2>
-      <a href="/quiz">くいずをやる</a>
+    <div className="top-container center-align">
+      <h4 className="top-header">
+        クイズを解いて
+        <br />
+        圧倒的成長！
+      </h4>
+      <div>
+        <Link to="/quiz" className="btn-large pink">
+          クイズをやる
+        </Link>
+      </div>
     </div>
   );
 };
